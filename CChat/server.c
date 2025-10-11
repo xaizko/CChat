e2fa245b4711392583eaf5233c3651a83581a504
xaizko <tonytran177013@gmail.com>
2025-10-11 15:43:30 -0400

    addedd cli argument check

--- CChat/server.c
+++ CChat/server.c
@@ -7,14 +7,21 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#define ADDRESS 127.0.0.1
-#define PORT 8080
+int main(int argc, char *argv[]) {
+    // Check for proper usage
+    if (argc < 4) {
+	fprintf(stderr, "Usage: %s <address> <port> <max clients>\n", argv[0]);
+	return 1;
+    }
 
-int main() {
-    int server_file_descriptor;
+    // Initialize necessary variables
+    const char *ADDRESS = argv[1];
+    const int PORT = atoi(argv[2]);
+    const int MAX_CLIENTS = atoi(argv[3]);
+
 
     // Create socket
-    server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
+    const int server_file_descriptor = socket(AF_INET, SOCK_STREAM, 0);
     if (server_file_descriptor < 0) {
 	fprintf(stderr, "Failed to create socket\n");
 	close(server_file_descriptor);
